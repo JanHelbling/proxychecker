f9d12053e006df6565579262ea1c7585ad528b84
Jan Helbling <jan.helbling@gmail.com>
2013-10-23 22:57:45 +0200

    added detailed license information

--- proxychecker.py
+++ proxychecker.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 #
-# (C) 2013 by Jan Helbling
-# jan.helbling@gmail.com
-# License: GNU/GPLv3 or newer http://www.gnu.org/licenses/gpl-3.0.txt
-# Comment: Just another ProxyChecker in Python
-# THIS PROGRAMM IS FREE SOFTWARE (GNU/GPLv3)
+#    proxychecker.py is a hitfaker and proxychecker
+#
+#    Copyright (C) 2013 by Jan Helbling <jan.helbling@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 import urllib.request
