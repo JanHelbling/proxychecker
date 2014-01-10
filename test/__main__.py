27b763e3d92bde3e84334f52f6bb2a06eb5a4a0b
Jan Helbling <jan.helbling@gmail.com>
2014-01-10 06:01:31 +0000

    Added a better test :)

--- test/__main__.py
+++ test/__main__.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 
-import os
+from os import execvp,fork,waitpid,WEXITSTATUS
+from sys import stderr,exit
 
 if __name__ == "__main__":
-	os.execvp("bin/proxychecker",["bin/proxychecker","-i","data/proxys.lst","-o","/dev/null"])
-
+	pid	=	0
+	try:
+		pid	=	fork()
+		if not pid:
+			print("./bin/proxychecker.py -i data/proxys.lst -o /dev/null -p 10 -t 2")
+			execvp("bin/proxychecker",["bin/proxychecker","-i","data/proxys.lst","-o","/dev/null","-p","10","-t","2"])
+	except KeyboardInterrupt as e:
+		stderr.write("[CTRL+C , KeyboardInterrupt!]")
+		exit(1)
+	except OSError as e:
+		stderr.write(e.filename+": "+e.strerror)
+		exit(1)
+	pid,st	= waitpid(pid,0)
+	st	= WEXITSTATUS(st)
+	exit(st)
