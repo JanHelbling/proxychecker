9cd173ea8cf075d913f28da05e45dc32b84be834
Jan Helbling <jan.helbling@gmail.com>
2014-01-13 01:50:17 +0100

    removed invalid Strings from ProxyChecker/__init__.py => __all__, added point to a errormsg

--- ProxyChecker/__init__.py
+++ ProxyChecker/__init__.py
@@ -17,4 +17,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-__all__ = ["RED","REDBOLD","GREEN","GREENBOLD","YELLOW","NOCOLOR","proxyregex","useragent","useragent_mobile","useragent_all","proxychecker"]
+__all__ = ["proxychecker"]
