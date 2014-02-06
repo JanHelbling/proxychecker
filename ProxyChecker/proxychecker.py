6e1280941d2b2509fb5ac659d1498c0b2113cd6c
Jan Helbling <jan.helbling@gmail.com>
2014-02-06 09:34:07 +0100

    i forgot to remove .format(lang) of automated buggy localedetection, that i removed in the last commit

--- ProxyChecker/proxychecker.py
+++ ProxyChecker/proxychecker.py
@@ -27,10 +27,10 @@ from os import path
 from platform import system
 from gettext import gettext as _
 
-if path.exists("/usr/share/locale/de/LC_MESSAGES/proxychecker.mo".format(lang)):
+if path.exists("/usr/share/locale/de/LC_MESSAGES/proxychecker.mo"):
 	gettext.bindtextdomain('proxychecker', '/usr/share/locale')
 	gettext.textdomain('proxychecker')
-elif path.exists("/share/locale/de/LC_MESSAGES/proxychecker.mo".format(lang)):
+elif path.exists("/share/locale/de/LC_MESSAGES/proxychecker.mo"):
 	gettext.bindtextdomain('proxychecker', '/share/locale')
 	gettext.textdomain('proxychecker')
 
